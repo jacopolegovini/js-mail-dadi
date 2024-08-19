d3984a55cfa2805816aed050ba3f17564b509565
jacopolegovini <legovini96@gmail.com>
2024-08-19 15:08:59 +0200

    gioco dei dadi completed

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 console.log('js ok')
+
+const playerThrowField = document.getElementById('player-throw');
+const pcThrowField = document.getElementById('pc-throw');
+const diceResultField = document.getElementById('dice-result');
+
+let playerThrowElement = Math.floor((Math.random() * 6) + 1);
+let pcThrowElement = Math.floor((Math.random() * 6) + 1);
+
+let result = '';
+
+if (playerThrowElement > pcThrowElement) {
+    result = 'Il giocatore ha vinto!'
+} else if (playerThrowElement < pcThrowElement) {
+    result = 'Il computer ha vinto!'
+} else {
+    result = 'È un pareggio!'
+}
+
+playerThrowField.innerText = playerThrowElement;
+pcThrowField.innerText = pcThrowElement;
+
+diceResultField.innerText = result
