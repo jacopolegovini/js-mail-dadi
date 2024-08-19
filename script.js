1e2b677822a8c986a564f8559237a0dad20ad737
jacopolegovini <legovini96@gmail.com>
2024-08-19 15:41:34 +0200

    elaboration issue fixed

--- script.js
+++ script.js
@@ -18,25 +18,42 @@ if (playerThrowElement > pcThrowElement) {
 playerThrowField.innerText = playerThrowElement;
 pcThrowField.innerText = pcThrowElement;
 
-diceResultField.innerText = result
+diceResultField.innerText = result;
 
 
 // ----------------------------------------------------------------
 
 
-// Dichiarazione variabili e DOM
+// Dichiarazione variabili e recupero elementi DOM
 const mail = document.getElementById('mail');
 const logIn = document.getElementById('login-button');
+const afterCheck = document.getElementById('after-check');
+
 
 const authorizedMail = ['jacopolegovini@gmail.com', 'carlopiano@gmail.com', 'tizioacaso@gmail.com', 'altrotizioacaso@gmail.com']
+let message = '';
 
 
 // Fase di input
 logIn.addEventListener('click', function(){
-    let mailCheck = mail.value;
+    let mailCheck = mail.value.trim();
     console.log(mailCheck)
+    
+    // Fase di elaborazione
+    for (let i = 0; i < authorizedMail.length; i++) {
+        if (mailCheck === authorizedMail[i]) {
+            message = 'Benvenuto!';
+            afterCheck.innerText = message
+            return;
+        } else {
+            message = 'Non sei autorizzato ad entrare. Prova con un\'altra mail.'
+            console.log(message)
+            afterCheck.innerText = message
 
+        }
+    }
 })
 
 
-// Fase di elaborazione
+
+
