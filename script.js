0b0a448aa3538a13114ad4f0b9e5b96a48b19807
jacopolegovini <legovini96@gmail.com>
2024-08-19 16:33:29 +0200

    exercise completed - button added dice throw

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 const playerThrowField = document.getElementById('player-throw');
 const pcThrowField = document.getElementById('pc-throw');
 const diceResultField = document.getElementById('dice-result');
+const buttonThrow = document.getElementById('button-throw');
 
-let playerThrowElement = Math.floor((Math.random() * 6) + 1);
-let pcThrowElement = Math.floor((Math.random() * 6) + 1);
+buttonThrow.addEventListener('click', function(){
 
-let result = '';
+    let playerThrowElement = Math.floor((Math.random() * 6) + 1);
+    let pcThrowElement = Math.floor((Math.random() * 6) + 1);
+    
+    let result = '';
+    
+    if (playerThrowElement > pcThrowElement) {
+        result = 'Il giocatore ha vinto!'
+    } else if (playerThrowElement < pcThrowElement) {
+        result = 'Il computer ha vinto!'
+    } else {
+        result = 'È un pareggio!'
+    }
+    
+    playerThrowField.innerText = playerThrowElement;
+    pcThrowField.innerText = pcThrowElement;
+    
+    diceResultField.innerText = result;
+})
 
-if (playerThrowElement > pcThrowElement) {
-    result = 'Il giocatore ha vinto!'
-} else if (playerThrowElement < pcThrowElement) {
-    result = 'Il computer ha vinto!'
-} else {
-    result = 'È un pareggio!'
-}
-
-playerThrowField.innerText = playerThrowElement;
-pcThrowField.innerText = pcThrowElement;
-
-diceResultField.innerText = result;
 
 
 // ----------------------------------------------------------------
